6142d0417cba44f0016384e64299dbc3ef603c31
Nils Reichel <nreichel@schueler.hakzell.at>
2025-12-23 08:41:36 +0100

    Projektdateien hinzufügen.

---
+++ WordGame/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "WordGame")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "WordGame.Pages")]
