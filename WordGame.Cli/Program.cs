642c9b6de2d8d890275826adcec445789ed6360a
Nils Reichel <nreichel@schueler.hakzell.at>
2026-02-03 09:20:36 +0100

    add view and viewmodel

--- WordGame.Cli/Program.cs
+++ WordGame.Cli/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using WordGame.Core;
+
+Console.WriteLine("Willkommen zum WordGame");
+Console.WriteLine("Drücken Sie ENTER, um zu beenden.");
+
+Game game = new Game();
+
+string word = "START";
+
+while (word != "")
+{
+    Console.Write("Wort: ");
+    word = Console.ReadLine();
+
+    if (word != "")
+    {
+        bool result = game.Add(word);
+
+
+        if (result == true)
+        {
+            Console.WriteLine("Super!");
+            Console.WriteLine("Aktuelle Kette: " + game.Get);
+        }
+        else
+        {
+            Console.WriteLine("Oje, das war falsch");
+            Console.WriteLine("Aktuelle Kette: " + game.Get);
+        }
+    }
+}
---
+++ WordGame.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace WordGame.Core.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private string _output = string.Empty;
+
+    [ObservableProperty]
+    private string _input = string.Empty;
+
+    [RelayCommand]
+    private void Add()
+    {
+
+    }
+}
